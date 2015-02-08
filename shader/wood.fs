d4a5d1d574956394f7830ad1222548e9c791d528
Gerold31 <kramer.leroy@gmail.com>
2015-02-08 14:55:00 +0100

    Update wood shader

--- shader/wood.fs
+++ shader/wood.fs
@@ -3,15 +3,14 @@
 in vec3 fpos;
 in vec3 fcolor;
 in vec3 fnormal;
+in float fseed;
 
 out vec4 pcolor;
 
-
 float rand(vec3 x)
 {
-	// @todo get this from outside
+	int seed = int(fseed + .5);
 
-	int seed = 0;
 	int n = seed + int(x.x * 57. + x.y * 83. + x.z * 127.);
 	n = (n<<13) ^ n;
 	return ( 1.0 - ( (n * (n * n * 15731 + 789221) + 1376312589) & 0x7fffffff) / 1073741824.0);
@@ -52,11 +51,10 @@ float interpolateNoise(vec3 x)
 	return interpolate(r0, r1, x.x);
 }
 
-float noise(vec3 x)
+float noise(vec3 x, float octaves, float persistance)
 {
-	// @todo get this from outside
-	float octaves = 4;
-	float persistance = .15;
+	//float octaves = 4;
+	//float persistance = .15;
 
 	float total = 0;
 
@@ -79,25 +77,24 @@ float noise(vec3 x)
 
 void main()
 {
-	// @todo get this from outside
-	vec3 base = vec3(150/256., 100/256., 50/256.);
+	vec3 base = fcolor;
 	vec3 base_min = 0.9*base;
 	vec3 base_max = 1.1*base;
 
-	vec3 v = vec3(fpos.x*2, fpos.y/5., fpos.z*2);
+	vec3 v = vec3(fpos.x/20, fpos.y/100, fpos.z/20);
 
-	float grain = (noise(v) + 1) * 10;
+	float grain = (noise(v, 5, .2) + 1) * 60;
 	grain = grain - int(grain);
 
-	float t = .5;
+	float t = .8;
 
 	if(grain < t)
 		grain = grain/t;
 	else
 		grain = (1-grain)/(1-t);
 
-	grain = pow(cos(grain), 3);
-//	grain = 1-pow(grain, 2);
+//	grain = pow(cos(grain), 3);
+	grain = 1-pow(grain, 2);
 //	grain = 1-pow(2, -5 * pow(grain, 2));
 
 	vec3 color = vec3(interpolate(base_min.r, base_max.r, grain), interpolate(base_min.g, base_max.g, grain), interpolate(base_min.b, base_max.b, grain));
--- shader/wood.vs
+++ shader/wood.vs
@@ -3,10 +3,12 @@
 in vec3 pos;
 in vec3 color;
 in vec3 normal;
+in float seed;
 
 out vec3 fpos;
 out vec3 fcolor;
 out vec3 fnormal;
+out float fseed;
 
 uniform mat4 proj;
 uniform mat4 model;
@@ -15,5 +17,6 @@ void main() {
 	fcolor = color;
 	fnormal = normal;
 	fpos = pos;
+	fseed = seed;
 	gl_Position = proj * model * vec4(pos, 1.0);
 }
