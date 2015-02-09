b1493307343d94e398aaf895eda57201fdab2b7f
Gerold31 <kramer.leroy@gmail.com>
2015-02-09 22:19:56 +0100

    Update objLoader to work with normals

--- src/objLoader.cpp
+++ src/objLoader.cpp
@@ -2,6 +2,10 @@
 
 #include "logging.h"
 
+#include <regex>
+
+std::regex regex("f (\\d+)//(\\d+) (\\d+)//(\\d+) (\\d+)//(\\d+)");
+
 void loadObj(const std::string &fileName,
 			 std::vector<glm::vec4> &vertices,
 			 std::vector<glm::vec3> &vertexNormals,
@@ -15,6 +19,9 @@ void loadObj(const std::string &fileName,
 		SEVERE("unable to load ./models/%s.obg",  fileName.c_str());
 		return;
 	}
+
+
+	std::vector<GLushort> nIndices;
 
 	std::string line;
 	int lineCount = 0;
@@ -42,14 +49,20 @@ void loadObj(const std::string &fileName,
 		else if (line.substr(0,2) == "f ") /* Faces */
 		{
 		  std::istringstream s(line.substr(2));
-		  GLushort a,b,c;
-		  s >> a; s >> b; s >> c;
-		  a--; b--; c--;
-		  vIndices.push_back(a); vIndices.push_back(b); vIndices.push_back(c);
+		  GLushort a,b,c,d,e,f;
+
+		  std::cmatch m;
+
+		  std::regex_match (line.c_str(), m, regex);
+
+		  vIndices.push_back(atoi(m[1].str().c_str())-1); vIndices.push_back(atoi(m[3].str().c_str())-1); vIndices.push_back(atoi(m[5].str().c_str())-1);
+		  nIndices.push_back(atoi(m[2].str().c_str())-1); nIndices.push_back(atoi(m[4].str().c_str())-1); nIndices.push_back(atoi(m[6].str().c_str())-1);
 		}
 		else if (line[0] == '#') { /* Comment */ }
 		else { /* anything else */ }
 	}
+
+
 
 // this is for surface normals, if needed
 //	normals.resize(vertices.size(), glm::vec3(0.0, 0.0, 0.0));
