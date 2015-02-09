edd660cc034c0929e7053779b04bb1c0380cd1c8
Eeep <dewa94@gmx.net>
2015-02-09 19:55:36 +0100

    Revert "Update mesh"
    This reverts commit 8e03d2b1161b4f92fa818afd3d99e2c91e4e2544.

--- src/mesh.cpp
+++ src/mesh.cpp
@@ -1,20 +1,8 @@
 #include "mesh.h"
 
-#include "objLoader.h"
-
-Mesh::Mesh(const std::string &fileName)
+Mesh::Mesh()
 {
-	std::vector<glm::vec4> positions;
-	std::vector<glm::vec3> normals;
-	std::vector<glm::vec2> texCoords;
-	std::vector<GLushort> indices;
 
-	loadObj(fileName, positions, normals, texCoords, indices);
-
-	mVertices.mPositions = &positions;
-	mVertices.mNormals = &normals;
-	mVertices.mTextureCoordinates = &texCoords;
-	mVertices.mIndices = &indices;
 }
 
 Mesh::~Mesh()
--- src/meshinstance.cpp
+++ src/meshinstance.cpp
@@ -1,61 +1,12 @@
 #include "meshinstance.h"
 
-MeshInstance::MeshInstance(Mesh &mesh)
+MeshInstance::MeshInstance()
 {
-	glGenBuffers(NUMBUFFERS, mVertexArrayBuffers);
 
-	glEnableVertexAttribArray(0);
-	glBindBuffer(GL_ARRAY_BUFFER, mVertexArrayBuffers[POSITIONVB]);
-	glVertexAttribPointer(
-		0,					// attribute: vertex position
-		4,                  // (x,y,z,w)
-		GL_FLOAT,           // type of elements
-		GL_FALSE,           // take values as-is
-		0,                  // no extra data between each position
-		0                   // offset of first element
-	);
-
-	glEnableVertexAttribArray(1);
-	glBindBuffer(GL_ARRAY_BUFFER, mVertexArrayBuffers[TEXCOORDVB]);
-	glVertexAttribPointer(
-		1,					// attribute: texture coordinate
-		2,					// (x,y)
-		GL_FLOAT,			// type of elements
-		GL_FALSE,			// take value as-is
-		0,					// no extra data between each position
-		0					// offset of first element
-		);
- 
-	glEnableVertexAttribArray(2);
-	glBindBuffer(GL_ARRAY_BUFFER, mVertexArrayBuffers[NORMALVB]);
-	glVertexAttribPointer(
-		2,					// attribute: vertex normal
-		3,                  // (x,y,z)
-		GL_FLOAT,           // type of elements
-		GL_FALSE,           // take values as-is
-		0,                  // no extra data between each position
-		0                   // offset of first element
-	);
- 
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, mVertexArrayBuffers[INDEXVB]);	
 }
 
 MeshInstance::~MeshInstance()
 {
-	glDeleteBuffers(NUMBUFFERS, mVertexArrayBuffers);
+
 }
 
-void MeshInstance::draw(const Camera &camera) const
-{
-	int size;
-	glGetBufferParameteriv(GL_ELEMENT_ARRAY_BUFFER, GL_BUFFER_SIZE, &size);  
-	glDrawElements(
-		GL_TRIANGLES,					// mode
-		size/sizeof(GLushort),			// draw count
-		GL_UNSIGNED_SHORT,				// taype of indices
-		&mVertexArrayBuffers[INDEXVB]);	// pointer to location where Indices are stored
-}
-
-void MeshInstance::update(float elapsedTime)
-{
-}
