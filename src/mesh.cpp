267645bac847e9a4c474499d10322ca5f5ec6354
Gerold31 <kramer.leroy@gmail.com>
2015-02-09 19:39:40 +0100

    Add meshloader

--- src/mesh.cpp
+++ src/mesh.cpp
@@ -1,8 +1,23 @@
 #include "mesh.h"
 
-Mesh::Mesh()
-{
+#include "objLoader.h"
 
+Mesh::Mesh(const std::string &fileName)
+{
+	/*
+	std::vector<glm::vec4> positions;
+	std::vector<glm::vec3> normals;
+	std::vector<glm::vec2> texCoords;
+	std::vector<GLushort> indices;
+	*/
+
+	loadObj(fileName, mVertices.mPositions, mVertices.mNormals, mVertices.mTextureCoordinates, mVertices.mIndices);
+/*
+	mVertices.mPositions = positions;
+	mVertices.mNormals = normals;
+	mVertices.mTextureCoordinates = texCoords;
+	mVertices.mIndices = indices;
+	*/
 }
 
 Mesh::~Mesh()
